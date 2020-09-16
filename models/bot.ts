f06532069be464b0b010c4e603a9cd0a20ebfb63
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-09-16 16:07:48 -0300

    Primeiro commit

---
+++ models/bot.ts
@@ -0,0 +1,83 @@
+﻿// Se o projeto não compilar, pelo erro Cannot find name 'File', precisa
+// acrescentar a lib DOM ao array compilerOptions.lib no arquivo tsconfig.json
+import AssistantV2 = require("ibm-watson/assistant/v2");
+import { IamAuthenticator } from "ibm-watson/auth";
+import appsettings = require("../appsettings");
+import Sql = require("../infra/sql");
+import Resposta = require("./resposta");
+
+export = class Bot {
+	public static async iniciarConversa(): Promise<{ idconversa: string, resposta: string }> {
+		// Pega o idconversa da API da IBM, e a resposta da mensagem de boas vindas,
+		// que deveria ser o id de um assunto...
+		const assistant = new AssistantV2({
+			version: appsettings.assistantVersion,
+			authenticator: new IamAuthenticator({
+				apikey: appsettings.serviceCredentials.apikey,
+			}),
+			url: appsettings.serviceCredentials.url,
+		});
+
+		const respostaSessao = await assistant.createSession({ assistantId: appsettings.assistantId });
+
+		const idconversa = respostaSessao.result.session_id;
+
+		// Como a conversa é simples, o diálogo só tem um nível de profundidade,
+		// daria para utilizar a versão sem estado, mais simples???
+		// https://cloud.ibm.com/apidocs/assistant/assistant-v2?code=node#send-user-input-to-assistant-stateless
+		// https://cloud.ibm.com/apidocs/assistant/assistant-v2?code=node#send-user-input-to-assistant-stateful
+		const respostaMensagem = await assistant.message({
+			assistantId: appsettings.assistantId,
+			sessionId: idconversa,
+			input: { message_type: "text", text: "" }
+		});
+
+		const resposta = ((respostaMensagem.result && respostaMensagem.result.output && respostaMensagem.result.output.generic && respostaMensagem.result.output.generic[0] && respostaMensagem.result.output.generic[0].text) || "");
+
+		return {
+			idconversa: idconversa,
+			resposta: resposta || "Olá!"
+		};
+	}
+
+	public static async enviarMensagem(idconversa: string, mensagem: string): Promise<string> {
+		// Envia a mensagem para a API da IBM, usando idconversa,
+		// e pega a resposta, que deveria ser o id de um assunto...
+		const assistant = new AssistantV2({
+			version: appsettings.assistantVersion,
+			authenticator: new IamAuthenticator({
+				apikey: appsettings.serviceCredentials.apikey,
+			}),
+			url: appsettings.serviceCredentials.url,
+		});
+
+		// Como a conversa é simples, o diálogo só tem um nível de profundidade,
+		// daria para utilizar a versão sem estado, mais simples???
+		// https://cloud.ibm.com/apidocs/assistant/assistant-v2?code=node#send-user-input-to-assistant-stateless
+		// https://cloud.ibm.com/apidocs/assistant/assistant-v2?code=node#send-user-input-to-assistant-stateful
+
+		let resposta: string = null;
+
+		try {
+			const respostaMensagem = await assistant.message({
+				assistantId: appsettings.assistantId,
+				sessionId: idconversa,
+				input: { message_type: "text", text: mensagem }
+			});
+
+			resposta = ((respostaMensagem.result && respostaMensagem.result.output && respostaMensagem.result.output.generic && respostaMensagem.result.output.generic[0] && respostaMensagem.result.output.generic[0].text) || "");
+
+			// Loga só o que não gerou uma exceção
+			await Sql.conectar(async (sql: Sql) => {
+				await sql.query("insert into chatlog (data, idconversa, conteudo) value (now(), ?, ?)", [idconversa, mensagem]);
+			});
+		} catch (e) {
+			if (e.status == 404 || e.message == "Invalid Session")
+				resposta = "-1";
+			else
+				throw e;
+		}
+
+		return resposta || "Não sei o que dizer sobre isso 😥\nPoderia falar de novo, por favor, de outra forma? 😊";
+	}
+};
---
+++ routes/api/bot.ts
@@ -0,0 +1,24 @@
+﻿import express = require("express");
+import wrap = require("express-async-error-wrapper");
+import jsonRes = require("../../utils/jsonRes");
+import Bot = require("../../models/bot");
+
+const router = express.Router();
+
+// Se utilizar router.xxx() mas não utilizar o wrap(), as exceções ocorridas
+// dentro da função async não serão tratadas!!!
+router.get("/iniciarConversa", wrap(async (req: express.Request, res: express.Response) => {
+	res.json(await Bot.iniciarConversa());
+}));
+
+router.post("/enviarMensagem", wrap(async (req: express.Request, res: express.Response) => {
+	const idconversa = req.query["idconversa"] as string;
+	const mensagem = ((req.body && req.body.mensagem) || "").toString().normalize().trim();
+	if (!idconversa || !mensagem) {
+		jsonRes(res, 400, "Dados inválidos");
+		return;
+	}
+	res.json(await Bot.enviarMensagem(idconversa, mensagem));
+}));
+
+export = router;
